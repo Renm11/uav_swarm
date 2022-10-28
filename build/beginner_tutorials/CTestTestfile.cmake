ca8cc8205c51337091f3703d25617adedfdfcd72
任明 <1689119010@qq.com>
2022-10-28 22:07:46 +0800

    Delete build directory

--- build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lab/work/ws/src/beginner_tutorials
-# Build directory: /home/lab/work/ws/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
